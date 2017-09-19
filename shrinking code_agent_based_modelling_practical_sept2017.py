583ea9e9594cd8be9b508c4ac1968c8133def7e7
jendmurphy <32068518+jendmurphy@users.noreply.github.com>
2017-09-19 21:02:25 +0100

    Adding in investigating boundary issues

--- shrinking code_agent_based_modelling_practical_sept2017.py
+++ shrinking code_agent_based_modelling_practical_sept2017.py
@@ -54,6 +54,42 @@ for i in range (num_of_agents):
     matplotlib.pyplot.scatter(agents[i][0],agents[i][1])
 matplotlib.pyplot.show()
 
+"""
+INvestigating Boundary Issues
+"""
+# blur ---------------------------------------
+import matplotlib.pyplot
+
+data = []
+processed_data = []
+
+# Fill with random data.
+for i in (range(0,99)):
+    datarow = []
+    for j in (range(0,99)):
+        datarow.append(random.randint(0,255))
+    data.append(datarow)
+
+# Blur.
+for i in (range(1,98)):
+    datarow = []
+    for j in (range(1,98)):
+        sum = data[i][j]
+        sum += data[i-1][j]
+        sum += data[i+1][j]
+        sum += data[i][j+1]
+        sum += data[i][j-1]
+        sum /= 5
+    datarow.append(sum)
+    processed_data.append(datarow)
+
+matplotlib.pyplot.imshow(data)
+matplotlib.pyplot.show()
+matplotlib.pyplot.imshow(processed_data)
+matplotlib.pyplot.show()
+
+# End ---------------------------------------
+
 
 
 
