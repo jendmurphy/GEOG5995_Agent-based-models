0baca5d713ebed312deebdcf2fd1a74515707a29
Jennifer Murphy <gyjdm@leeds.ac.uk>
2017-09-19 16:11:48 +0100

    INput code for area of a circle

--- learningpython.py
+++ learningpython.py
@@ -61,3 +61,9 @@ b = "one line."
 c = "\nThis is a second."
 
 print(a, b, c)
+
+#Code for an input 
+response = input("What is your radius? ")
+r = float(response)
+area = 3.14159 * r**2
+print("The area is ", area)
