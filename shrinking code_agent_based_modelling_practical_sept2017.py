f0d01bcbd662954170703b74e1f69d252c2fb61c
jendmurphy <32068518+jendmurphy@users.noreply.github.com>
2017-09-19 18:20:49 +0100

    Train coding

--- shrinking code_agent_based_modelling_practical_sept2017.py
+++ shrinking code_agent_based_modelling_practical_sept2017.py
@@ -7,16 +7,53 @@ This is a temporary script file.
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 19 10:24:31 2017
-CONTAINERS:  AGENT BASED MODELLING PRACTICAL
+SHRINKING CODE:  AGENT BASED MODELLING PRACTICAL
 @author:Jen Murphy
-change test
+
 """
 # import random
 import random
 import operator
 import matplotlib.pyplot
+#Add a variable to control the number of agents
+num_of_agents = 10
+
 #Create a list for agents
 agents = []
+
+#Use a for loop to create a number of agents within the list agents
+for i in range(num_of_agents):
+    agents.append([random.randint(0,100),random.randint(0,100)])
+
+print (agents)
+
+# Now to optimise the random movement code
+# To get x coordinates for agent i
+for i in range(num_of_agents):
+   if random.random() < 0.5:
+       agents[i][0] += 1
+       else :
+           agents[i][0] -= 1
+#  To get y coordinates for agent i
+for i in range(num_of_agents):
+    if random.random() < 0.5:
+        agents[i][1] += 1
+        else:
+            agents[i][1] -= 1
+
+#PLot the agents
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.ylim(0, 99)
+for i in range (num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][0],agents[i][1])
+matplotlib.pyplot.show()
+
+
+
+
+
+"""
+Last practical's code
 
 #AGENT 0
 x0 = random.randint(0,100)
@@ -59,3 +96,5 @@ matplotlib.pyplot.scatter(agents[0][0],agents[0][1], color = 'red')
 matplotlib.pyplot.scatter(agents[1][0],agents[1][1], color = 'blue')
 matplotlib.pyplot.scatter(agents[2][0],agents[2][1], color = 'green') 
 matplotlib.pyplot.show()
+
+"""
