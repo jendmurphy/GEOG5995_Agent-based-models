9bc7fa18c624c54a519b512f071bf4f4ac7491bc
jendmurphy <32068518+jendmurphy@users.noreply.github.com>
2017-09-20 08:56:54 +0100

    Practical 2 solution
    Use pyplot to plot the actors in 2D space.  Append coordinates to a list agent by agent, or in some shorter code.

--- GEOG5995_jendmurphy.py
+++ GEOG5995_jendmurphy.py
@@ -7,6 +7,56 @@ AGENT BASED MODELLING PRACTICAL
 """
 # import random
 import random
+import operator
+import matplotlib.pyplot
+
+#Create a list for agents
+agents = []
+
+#AGENT 0
+x0 = random.randint(0,100)
+y0 = random.randint(0,100)
+
+agents.append([x0,y0])
+print(agents)
+
+#AGENT 1
+x1 = random.randint(0,100)
+y1 = random.randint(0,100)
+
+agents.append([x1,y1])
+print(agents)
+
+#Alternatively, we can do this without ever creating x or y.
+agents.append([random.randint(0,100),random.randint(0,100)])
+print(agents)
+
+"""
+max is a function which will look at the first item, and only look at the 
+second if there is a tie - so it ignores the y coordinate in most
+cases here
+"""
+print(max(agents))
+
+"""
+operator.itemgetter looks at item 1 in each pair in the list - remember that 
+numbering starts at zero so this is the y coordinate.  Now we see the maximum
+y coordinate
+"""
+print(max(agents, key=operator.itemgetter(1)))
+
+"""
+Then plot the agent locations using a library package which plots
+"""
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.scatter(agents[0][0],agents[0][1], color = 'red')
+matplotlib.pyplot.scatter(agents[1][0],agents[1][1], color = 'blue')
+matplotlib.pyplot.scatter(agents[2][0],agents[2][1], color = 'green') 
+matplotlib.pyplot.show()
+
+"""
+PRACTICAL 1 CODE NOT USED HERE
 
  # Make a x variable.
 x0 = 50
@@ -90,4 +140,4 @@ xj = 0
 yj = 0
 distance_ij = (((xi-xj)**2)+((yi-yj)**2))**0.5
 print (distance_ij)
-
+"""
